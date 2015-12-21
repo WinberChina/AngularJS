b589abbba206c062b6c4787c8eba6f7293633f6c
WangDzh <wangdzh89@126.com>
2015-12-21 21:45:48 +0800

    Angular Directive
    添加Angular 指令Demo

---
+++ Directive/js/CompileAndLink.js
@@ -0,0 +1,12 @@
+var appModule = angular.module("app", []);
+appModule.directive("hello", function() {
+	return {
+		restrict: 'E',
+        template: '<span>Hi there</span>',
+        replace: true
+	}
+});
+
+appModule.controller('MyController',function($scope) {
+    $scope.things = [1,2,3,4,5,6];
+});
